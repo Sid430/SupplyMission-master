38ddf9c533d4bcc6812d563e06df047aff050dd2
Siddhartha Milkuri <sid@Siddharthas-Air.attlocal.net>
2020-10-08 16:42:56 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,85 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(1000, 500);
+	rectMode(CENTER);
+	
+	lineSprite=createSprite(500,490,190,25)
+	line2Sprite=createSprite(405,490,25,190)
+	line2Sprite=createSprite(590,490,25,190)
+
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+   
+	Matter.Body.setStatic(packageBody,false)
+
+  }
+}
+
+if (keyCode === LEFT_ARROW) {
+
+helicopterSprite.x=helicopterSprite.x-20
+translation={x:20,y:0}
+Matter.Body.translate(packageBody, {x:-20,y:0})
+
+
+
+
+
+
+
+
+
+
+}
